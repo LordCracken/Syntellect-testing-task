77ea95cdf5023ebb3c7afe72f71525c64d0e0bc1
LordCracken <vladislav070201@gmail.com>
2023-01-16 00:53:50 +0300

    use text controllers in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,61 @@
+import TextController from './components/TextController';
+
+import TextControllerStore from './store/TextControllerStore';
 import './App.css';
 
-function App() {
-  return <div></div>;
-}
+const firstTextCtrStore = new TextControllerStore('', [
+  {
+    id: 'b1',
+    text: 'Clear Input',
+    callback: () => {
+      firstTextCtrStore.changeContent('');
+    },
+  },
+  {
+    id: 'b2',
+    text: 'Set "Hello world!"',
+    callback: () => {
+      firstTextCtrStore.changeContent('Hello world!');
+    },
+  },
+]);
+const secondTextCtrStore = new TextControllerStore(
+  '',
+  [
+    {
+      id: 'b1',
+      text: 'Alert Content',
+      callback: () => {
+        const value = secondTextCtrStore.content;
+
+        if (value) {
+          alert(value);
+        }
+      },
+    },
+  ],
+  [
+    {
+      id: 'b2',
+      text: 'Alert Number',
+      callback: () => {
+        const value = secondTextCtrStore.content;
+
+        if (value && !isNaN(+value)) {
+          alert(value);
+        }
+      },
+    },
+  ],
+);
+
+const App = () => {
+  return (
+    <div>
+      <TextController store={firstTextCtrStore} />
+      <TextController store={secondTextCtrStore} />
+    </div>
+  );
+};
 
 export default App;
