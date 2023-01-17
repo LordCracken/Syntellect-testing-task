478386c64fd7c1f2396aa928ee9d170a6e59d243
LordCracken <vladislav070201@gmail.com>
2023-01-17 13:05:49 +0300

    prepare interfaces for autocomplete controller

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -14,3 +14,24 @@ export interface ITextController {
   leftButtons: IButtonConfigs[];
   changeContent: (newValue: string) => void;
 }
+
+export type IHint = Record<'name' | 'fullName' | 'flag', string>;
+
+export interface IAutocompleteController {
+  content: string;
+  hintsList: IHint[];
+  maxHints: number;
+  isLoading: boolean;
+  isTouched: boolean;
+  getHintsList: (country: string) => void;
+  setIsTouched: (value: boolean) => void;
+  changeContent: (newValue: string) => void;
+}
+
+export type IAutocompleteCtrComponent = Omit<IAutocompleteController, 'maxHints'>;
+
+export interface IHintsList {
+  list: IHint[];
+  isLoading: boolean;
+  hasValue: boolean;
+}
